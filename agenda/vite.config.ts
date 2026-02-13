69abaf07d72aa33c512997957b2caac89d314ceb
jaksa <jaksa76@gmail.com>
2026-02-13 17:48:52 +0000

    all apps use fleabox now

--- agenda/vite.config.ts
+++ agenda/vite.config.ts
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/agenda/',
+  build: {
+    outDir: 'dist/agenda',
+    emptyOutDir: true
+  },
   plugins: [react()],
 })
--- list/vite.config.ts
+++ list/vite.config.ts
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/list/',
+  build: {
+    outDir: 'dist/list',
+    emptyOutDir: true
+  },
   plugins: [react()],
 })
